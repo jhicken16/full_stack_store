b801047756b74a0c9a9a96a8293f5a27875a3a91
Jhicken16 <jhicken16@gmail.com>
2024-01-11 17:45:38 +0000

    controller/routes/auth.js swagger documentation added and route for login using passport. controller/routes/index.js auth inclueded in loader and app and passport passed to function to initiat route. controller/services/AuthService.js cleared extar spacing and used for some experimatation with database; experiments cleared nothing changed in logic. json packages update to inclue nodemnon in dev dependence and script run devstart to run nodemon.

--- controller/routes/auth.js
+++ controller/routes/auth.js
@@ -5,6 +5,46 @@ module.exports = (app, passport) => {
     
     app.use('/auth', router)
 
+    /**
+     * @swagger
+     * /auth/login:
+     *      post:
+     *          summary: logs in user
+     *          description: This is a path that logs the user in and stores related user information in sessions object
+     *          requestBody:
+     *              required: true
+     *              content: 
+     *                  application/json:
+     *                      schema:
+     *                          type: object
+     *                          properties:
+     *                              email:
+     *                                  type: string
+     *                              password:
+     *                                  type: string
+     *          responses:
+     *              "200":
+     *                  description: return ok if successful
+     *                  content:
+     *                      application/json:
+     *                          schema:
+     *                              type: object
+     *                              properties:
+     *                                  encoded: 
+     *                                      type: string
+     *              "401":
+     *                  description: incorrect username or password you are not authorized.
+     *                  content: 
+     *                      application/json:
+     *                          schema:
+     *                              type: string 
+     *              "404":
+     *                  description: user was not found
+     *                  content: 
+     *                      application/json:
+     *                          schema:
+     *                              type: string                   
+     */
     router.post('/login', passport.authenticate('local') ,async (request, response, next) => {
         
         const data = request.body
--- controller/routes/index.js
+++ controller/routes/index.js
@@ -1,6 +1,7 @@
 const products = require('./products')
-
+const auth = require('./auth')
 
 module.exports = (app, passport) => {
+    auth(app, passport)
     products(app)
 }

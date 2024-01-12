f48b0cfc99ffbb58b820f81109bc2263767dca0a
Jhicken16 <jhicken16@gmail.com>
2024-01-12 11:12:53 +0000

    register route added to controller/routes/auth.js input validated create acount for user password is hashed in cotroller/services/authenticate.js and added to the database.

--- controller/routes/auth.js
+++ controller/routes/auth.js
@@ -1,9 +1,75 @@
 const express = require('express')
 const router = express.Router()
 
+const { body, validationResult } = require('express-validator')
+
 module.exports = (app, passport) => {
     
     app.use('/auth', router)
+
+    /**
+     * @swagger
+     * /auth/register:
+     *      post:
+     *          summary: Creates account for user
+     *          description: excepts email, password and name, using this to create account for the user
+     *          requestBody:
+     *              required: true
+     *              content: 
+     *                  application/json:
+     *                      schema:
+     *                          type: object
+     *                          properties:
+     *                              email:
+     *                                  type: string
+     *                              password:
+     *                                  type: string
+     *                              name:
+     *                                  type: string
+     *          responses:
+     *              "200":
+     *                  description: return ok if successful, account has been created
+     *                  content:
+     *                      application/json:
+     *                          schema:
+     *                              type: string
+     *                              example: "ok" 
+     *              "409":
+     *                  description: Email is already in use
+     *                  content: 
+     *                      application/json:
+     *                          schema:
+     *                              type: string
+     *                              example: 'Email already in use.' 
+     *              "422":
+     *                  description: Input did not pass validation process
+     *                  content: 
+     *                      application/json:
+     *                          schema:
+     *                              type: string
+     *                              example: "Invalid input on [email || password || name]"              
+     */
+    router.post('/register',
+    [
+        body('email').notEmpty().isString().isEmail().blacklist('<>,./?!`"{(;:'),
+        body('name').notEmpty().isString().blacklist('<>,./?!`"{(;:'),
+        body('password').notEmpty().isString().blacklist('<>,./?!`"{(;:')
+    ] 
+    ,async (request, response, next) => {
+        const result = validationResult(request)
+        if ( !result.isEmpty()){
+            console.log(result)
+            return response.status(422).send(`Invalid input on ${result.path}`)
+        }
+        const data = request.body
+        try {
+            const res = await Authentication.register(data)
+            console.log(res)
+            response.status(200).send('ok')
+        } catch(err){
+            next(err)
+        } 
+    })
 
     /**
      * @swagger

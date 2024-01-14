6df3a4c70bcde1c433ce1ab99c1bcf39b02b9615
Jhicken16 <jhicken16@gmail.com>
2024-01-14 14:34:55 +0000

    swagger documentation seprated storing components in root directory to reduce size of comments above api's

--- controller/loaders/swagger.js
+++ controller/loaders/swagger.js
@@ -12,10 +12,12 @@ const swaggerOptions = {
         
     },
     //An array of were to find the api's; paths values.
-    apis: ['./controller/routes/products.js', './controller/routes/auth.js']
+    apis: [ './swaggerComponents.yaml', './controller/routes/products.js', './controller/routes/auth.js']
 }
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions)
+
+console.log(swaggerDocs)
 
 module.exports = (app) => {
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
